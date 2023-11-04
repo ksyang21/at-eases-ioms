76e0dd75673cbe305467ad3ea2a1a9dcbfce1eef
ksyang21 <ksyang1121@hotmail.com>
2023-11-04 17:00:34 +0800

    add package product model, migration

---
+++ database/migrations/2023_11_04_072543_create_package_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('package_products', function (Blueprint $table) {
+            $table->id();
+            $table->primary(['product_id', 'package_id']);
+            $table->foreign('package_id')->references('id')->on('packages')->cascadeOnDelete();
+            $table->foreign('product_id')->references('id')->on('products')->cascadeOnDelete();
+            $table->unsignedInteger('quantity');
+            $table->unsignedFloat('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('package_products');
+    }
+};
