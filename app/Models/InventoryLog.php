15f216f38f68eff51d14eeeaa920a921ad560135
ksyang21 <ksyang1121@hotmail.com>
2023-11-07 21:46:42 +0800

    add inventory logs migration, model

---
+++ app/Models/InventoryLog.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InventoryLog extends Model
+{
+    use HasFactory;
+
+    public $timestamps = true;
+
+    protected $fillable = [
+        'product_id',
+        'quantity',
+        'stock_status'
+    ];
+
+    public function product(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this->hasOne(Product::class, 'id', 'product_id');
+    }
+}
