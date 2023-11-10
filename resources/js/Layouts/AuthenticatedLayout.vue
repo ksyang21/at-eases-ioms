04287afa4df0e292d575d1d398ab49f6369e8e83
ksyang21 <ksyang1121@hotmail.com>
2023-11-10 20:46:16 +0800

    show success message

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,14 +1,28 @@
 <script setup>
-import {ref} from 'vue';
+import {computed, inject, ref} from 'vue';
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import {Link} from '@inertiajs/vue3';
+import {Link, usePage} from '@inertiajs/vue3';
 import Sidebar from "@/Components/Sidebar.vue";
 
 const showingNavigationDropdown = ref(false);
+
+const Swal = inject('$swal')
+const message = computed(() => usePage().props.alert.success)
+if (message.value) {
+    Swal.fire({
+        text: message.value,
+        icon: "success",
+        timer: 1500,
+        showConfirmButton: false,
+        allowEscapeKey: false,
+        position: 'top-end',
+        toast: true
+    })
+}
 </script>
 
 <template>
