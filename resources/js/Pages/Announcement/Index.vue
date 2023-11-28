5bacf96f355ed75e0eeab59b63e9ea220328ae36
ksyang21 <ksyang1121@hotmail.com>
2023-11-28 09:06:05 +0800

    indentation

--- resources/js/Pages/Announcement/Index.vue
+++ resources/js/Pages/Announcement/Index.vue
@@ -284,7 +284,7 @@ function removeAnnouncement(announcement) {
 													class="relative z-10 p-2 text-xs leading-none text-white whitespace-no-wrap bg-black shadow-lg">Remove</span>
 												<div class="w-3 h-3 -mt-2 rotate-45 bg-black"></div>
 											</div>
-										</div>
+ 										</div>
 									</td>
 								</tr>
 								</tbody>
