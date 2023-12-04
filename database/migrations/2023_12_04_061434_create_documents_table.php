e613886060132cab0268e78a4507ca4e65a83893
ksyang21 <ksyang1121@hotmail.com>
2023-12-04 14:38:17 +0800

    add document status column

--- database/migrations/2023_12_04_061434_create_documents_table.php
+++ database/migrations/2023_12_04_061434_create_documents_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,6 +15,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('filename')->default(NULL)->nullable();
             $table->unsignedBigInteger('uploaded_by');
+            $table->enum('status', ['active', 'inactive', 'outdated']);
             $table->foreign('uploaded_by')->references('id')->on('users')->noActionOnDelete();
             $table->timestamps();
         });
